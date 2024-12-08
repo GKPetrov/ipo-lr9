cf5e308590ad834ae8124dd448fffade8cbc0de8
Petrov Gleb <glebpetrof2007@gmail.com>
2024-12-08 17:59:03 +0300

    Task 4 complete

--- collision/Task4.py
+++ collision/Task4.py
@@ -1,13 +1,12 @@
-from Task2 import isCorrectRect
-from Task3 import isCollisionRect
+from collision import Task2, Task3
 class ValueError(Exception):
    pass
 def intersectionAreaRect(list1, list2):
-    if isCorrectRect:
-        if isCollisionRect:
+    if Task2.isCorrectRect(list1[0], list1[1]) and Task2.isCorrectRect(list2[0], list2[1]):
+        if Task3.isCollisionRect(list1, list2):
             res = abs((list2[1][0]-list1[0][0])*(list2[1][1]-list1[0][1]))
             return res
-        else:
+        if not Task3.isCollisionRect(list1, list2):
             return 0
     else:
         raise ValueError("Передан некорректный прямоугольник")
